8dcaaaa86d0c44f13c5998c0735a2d53d60d9f0c
Leonardodarienzo <darienzo.leonardo@iisgalvanimi.edu.it>
2023-02-14 11:59:46 +0000

    secondo_commit

--- flaskMilano.py
+++ flaskMilano.py
@@ -11,7 +11,7 @@ def ciao_mondo():
 
 @app.route('/benvenuto', methods=['GET'])
 def benvenuto():
-    return render_template('benvenuto.html')
+    return render_template('milano.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)#da fare sempre
+  app.run(host='0.0.0.0', port=3245, debug=True)
